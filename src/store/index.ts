719678a66070de297b0447cb53a5544c0591a51c
Nir Weiler <nirwe@wix.com>
2021-05-24 15:16:02 +0300

    Redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,42 +1,20 @@
-import { combineReducers } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import { configureStore, Action } from '@reduxjs/toolkit';
 import { ThunkAction } from 'redux-thunk';
 import { todosReducer } from './todosReducer'
 
-export interface todosState {
-  todos: string[]
-}
+// export const rootReducer = combineReducers({
+//   todos: todosReducer
+// });
 
-export interface showListState {
-  showList: boolean
-}
+// const store = configureStore({
+//  reducer: rootReducer
+// });
 
-export interface showInputState {
-  showInput: boolean
-}
+const store = createStore(todosReducer);
 
-const initialTodosState = {
-  todos: []
-}
-
-const initialShowListState = {
-  showList: true
-}
-
-const initialShowInputState = {
-  showInput: false
-}
-
-export const rootReducer = combineReducers({
-  todos: (state = {}, action) => ({})
-});
-
-const store = configureStore({
- reducer: rootReducer
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
-export type AppDispatch = typeof store.dispatch;
+//export type RootState = ReturnType<typeof rootReducer>;
+//export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
+//export type AppDispatch = typeof store.dispatch;
 
 export default store;
--- src/store/todosReducer.ts
+++ src/store/todosReducer.ts
@@ -1,3 +1,26 @@
-export const todosReducer = (state : string[] = [], action : string) => {
-    return [...state, action];
+export interface todosState {
+    todos: string[],
+    showList : boolean,
+    showInput : boolean
+  }
+
+  const initialTodosState = {
+    todos: [],
+    showList : true,
+    showInput : false
+  }
+
+  type Action = {type: string, payload : string}
+
+
+export const todosReducer = (state : todosState = initialTodosState, action : Action) => {
+    switch(action.type)
+    {
+        case "OPEN_INPUT":
+            return {...state, showList:false, showInput:true}
+        case "DONE":
+            return {...state, todos : [...state.todos, action.payload], showList:true, showInput:false}
+        default:
+            return state;
+    }
 }
